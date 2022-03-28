32e6eaa2dc2ce365444c2726c46ef519ac6ce8b5
Arad119 <11808560+Arad119@users.noreply.github.com>
2022-03-28 23:57:23 +0200

    Added autoupdating

--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,20 @@
 
 document.getElementById('dirs').addEventListener('click', (evt) => {
-    evt.preventDefault()
-    window.postMessage({
-      type: 'select-dirs',
-    })
+  evt.preventDefault()
+  window.postMessage({
+    type: 'select-dirs',
   })
+})
 
 
 window.electronAPI.filePathInput((event, value) => {
-  $('#folder-path').val(value);
+$('#folder-path').val(value);
 })
 
 
 const setButton = $('#btn')[0];
 
 setButton.addEventListener('click', () => {
-    const steamid = $('#steamid').val();
-    window.electronAPI.processDemos(steamid)
+  const steamid = $('#steamid').val();
+  window.electronAPI.processDemos(steamid)
 });
